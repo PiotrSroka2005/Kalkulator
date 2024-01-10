71f1f52dade8847172cd99a39a38085102ae5a7f
Bart1251 <bartlomiej.wiecek@vp.pl>
2024-01-10 11:29:32 +0100

    OnSelectNumber function creation

--- Kalkulator/Kalkulator/MainPage.xaml.cs
+++ Kalkulator/Kalkulator/MainPage.xaml.cs
@@ -10,9 +10,31 @@ namespace Kalkulator
 {
     public partial class MainPage : ContentPage
     {
+        string currentEntry = "";
+        string leftOperand = "";
+        string rightOperand = "";
+        string operation = "";
+        bool operationSelected = false;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void OnSelectNumber(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            if (operationSelected) 
+            {
+                rightOperand += button.Text;
+                currentEntry = rightOperand;
+            }
+            else
+            {
+                leftOperand += button.Text;
+                currentEntry = leftOperand;
+            }
+            Output_Label.Text = currentEntry;
         }
     }
 }
